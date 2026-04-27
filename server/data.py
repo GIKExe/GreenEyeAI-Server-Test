30d0d4c9cf3e0f21311888cf72f79a1c9d68c67f
GIKExe <72767917+GIKExe@users.noreply.github.com>
2026-04-27 12:39:49 +0300

    Любой тип хранимой информации

--- server/data.py
+++ server/data.py
@@ -1,22 +1,22 @@
 from typing import Any
 
 
-class Data(dict[str, Any]):
+class Data(dict[Any, Any]):
 	def __init__(self) -> None:
 		super().__init__()
 
-	def __getattr__(self, key: str) -> Any | None:
+	def __getattr__(self, key: Any) -> Any | None:
 		if key in self:
 			return super().__getitem__(key)
 		return None
 
-	def __setattr__(self, key: str, value: Any) -> None:
+	def __setattr__(self, key: Any, value: Any) -> None:
 		super().__setitem__(key, value)
 
-	def __getitem__(self, key: str) -> Any | None:
+	def __getitem__(self, key: Any) -> Any | None:
 		if key in self:	
 			return super().__getitem__(key)
 		return None
 	
-	def __setitem__(self, key: str, value: Any) -> None:
+	def __setitem__(self, key: Any, value: Any) -> None:
 		super().__setitem__(key, value)
